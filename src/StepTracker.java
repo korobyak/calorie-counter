5ab96e43e54be7f2dae19003254083543e642f1e
Korobyaka <akorobaka@gmail.com>
2023-11-12 23:33:38 +0300

    скелет

---
+++ src/StepTracker.java
@@ -0,0 +1,12 @@
+public class StepTracker {
+    MontData[] monthToData;
+    int goal = 1000;
+
+    public StepTracker() {
+        // заполнить массив месяцами, длина - 12
+    }
+
+    public static class MontData {
+        // создать массив с днями - 30 дней
+    }
+}
